f388fcd21c66e8871b0814de947f5ebe796fe40c
Mary Lin <25216651+themarylin90@users.noreply.github.com>
2018-08-20 00:19:36 -0700

    web scraping

---
+++ keys.py
@@ -0,0 +1,4 @@
+consumer_key = '1uODzz6zW4lSZNdKlenDXzkg8'
+consumer_secret = 'IESMsYz2aUqUJKmttvomsdQyXsIgoXWcMltbxnIYxEDKBR0jh0'
+access_token = '822488123321921538-4PrAu6gmvdlRgOULwlYQbMnkTkgdkny'
+access_token_secret = 'INqLyzm6AeLiNYmP1eL78aSWTzxpUq1IfMRvXz0l6Q1xi'
